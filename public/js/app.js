cec65e42ff433aa1b8e37efb99d1c714e9cff86f
BogdanAntonyuk <boba.tochka@gmail.com>
2020-03-06 14:17:14 +0200

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side javascript file is loaded');
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
@@ -8,21 +6,21 @@ const messageTwo = document.querySelector('#message-2');
 // messageOne.textContent = 'From JavaScript'
 
 weatherForm.addEventListener('submit', e => {
-   e.preventDefault();
+	e.preventDefault();
 
-   const location = search.value;
+	const location = search.value;
 
-   messageOne.textContent = 'Loading...';
-   messageTwo.textContent = '';
+	messageOne.textContent = 'Loading...';
+	messageTwo.textContent = '';
 
-   fetch('http://localhost:3000/weather?adress=' + location).then(response => {
-      response.json().then(data => {
-         if (data.error) {
-            messageOne.textContent = data.error;
-         } else {
-            messageOne.textContent = data.location;
-            messageTwo.textContent = data.forecast;
-         }
-      });
-   });
+	fetch('http://localhost:3000/weather?adress=' + location).then(response => {
+		response.json().then(data => {
+			if (data.error) {
+				messageOne.textContent = data.error;
+			} else {
+				messageOne.textContent = data.location;
+				messageTwo.textContent = data.forecast;
+			}
+		});
+	});
 });
